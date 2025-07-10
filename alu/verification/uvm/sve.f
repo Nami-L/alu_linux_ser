478524b1b594b7a656857ab9f2695d591bfeedb6
Nami-L <namigtle066@gmail.com>
2025-07-10 16:21:19 -0600

    FEAT: prueba final 2

--- alu/verification/uvm/sve.f
+++ alu/verification/uvm/sve.f
@@ -1,9 +1,4 @@
-+incdir+verification/uvm/env
-+incdir+verification/uvm/tests
-+incdir+verification/uvm/tb
-verification/uvm/env/top_env_pkg.sv
-verification/uvm/tests/top_test_pkg.sv
-verification/uvm/tb/tb.sv
+
 
 +incdir+$GIT_ROOT/alu/verification/uvm/env
 +incdir+$GIT_ROOT/alu/verification/uvm/tests
